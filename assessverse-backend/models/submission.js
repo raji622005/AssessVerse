a70bb29b3a149ac23ae8f3299018cf37466485a6
Raji <rajalakshmiaruna2005@gmail.com>
2026-04-20 15:45:39 +0530

    fix:make

--- assessverse-backend/models/submission.js
+++ assessverse-backend/models/submission.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema; // Add this line
 const submissionSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   assessmentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Assessment', required: true },
-  answers: [Schema.Types.Mixed],
+  answers: { type: Array, default: [] },
   
   
   score: { type: Number, default: 0,required: true }, 
